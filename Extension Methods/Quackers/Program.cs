865bb7a854324b77eb7d8016d679bb3e8130bba9
fortuin83 <bfortuin1@nait.ca>
2017-09-07 14:45:04 -0600

    Added the VS2015 Console program to make strings quack

---
+++ Extension Methods/Quackers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quackers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Brian Fortuin";
+
+            Console.WriteLine(name);
+            Console.WriteLine(name.Quack());
+
+
+
+        }
+
+        
+    }
+
+    public static class Extensions //NOthe that this is static
+    {
+        public static string Quack(this string self)
+        {
+            return self + " (quack)";
+        }
+
+    }
+}
